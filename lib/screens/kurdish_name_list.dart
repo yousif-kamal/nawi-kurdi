912fe2b96c1dce6ddf1dd19d5652fd060d372835
yousif-kamal <yousifbarber11@gmail.com>
2022-07-15 17:26:53 +0300

    init and datamodel

---
+++ lib/screens/kurdish_name_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class KurdishNamesLst extends StatelessWidget {
+  const KurdishNamesLst({Key? key}) : super(key: key);
+
+  //TODO create data model
+
+  //TODO create class for the kurdish names service
+
+  //TODO create a method to get the lis]t of kurdish names
+
+  //TODO render the list of users to the screen
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text("ddd"),
+          ),
+        ],
+      ),
+    );
+  }
+}
